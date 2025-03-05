61186e3ffe71f2c2ea9a9a26ea9a8d7e253d7d1a
Amit Prakash <prakashamit37@gmail.com>
2025-03-05 14:16:49 +0530

    Data Added to Data.js and Cards component started

---
+++ src/Components/Cards.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Cards = () => {
+    return(
+        <div className="card">
+            <section className="column-left">
+                
+            </section>
+        </div>
+    );
+};
+
+export default Cards;
---
+++ src/Data.js
@@ -0,0 +1,52 @@
+export const data = [
+	{
+		id: 1,
+		date: "May 2018",
+		title: "Kempty Falls, Mussoorie",
+		description: "Kempty Falls is a waterfall in Ram Gaon and at the south of Kempty, in the Tehri Garhwal District of Uttarakhand, India.[1] It is 13 kilometres (8 mi) from Mussoorie on the Chakrata Road, and 45 kilometres (28 mi) from Dehradun.[2] It is nearly 1364 meters above sea level, at 78°-02’East longitude and 30° -29’North latitude. The Kempty Falls, and the area around is surrounded by high mountain ranges at an altitude of 4500 feet.[3] There is an estimated tourist inflow to the Falls of over 10 lakh (one million).",
+		coverImg: "./images/Kempty_Falls.jpg",
+		state: "Uttrakhand",
+		googleMapLink: 
+"https://www.google.com/maps/place/Kempty+Falls/@30.4878339,78.0341178,17z/data=!3m1!4b1!4m6!3m5!1s0x3908d19963f63b51:0xaf4d748e3bc60a88!8m2!3d30.4878339!4d78.0366927!16s%2Fg%2F1pxxrv21z?entry=ttu&g_ep=EgoyMDI1MDMwMi4wIKXMDSoASAFQAw%3D%3D",
+	},
+	{
+		id: 2,
+		date: "December 2019",
+		title: "Mahakaleshwar Mandir, Ujjain",
+		description: "Mahakaleshwar Jyotirlinga is a Hindu temple dedicated to Shiva and is one of the twelve Jyotirlingas, shrines which are said to be the most sacred abodes of Shiva. It is located in the ancient city of Ujjain in the state of Madhya Pradesh, India. The temple is situated on the side of the holy river Shipra. The presiding deity, Shiva in the lingam form is believed to be Swayambhu, deriving currents of power (Shakti) from within itself as against the other images and lingams that are ritually established and invested with mantra-shakti.",
+		coverImg: "./images/Mahakal.jfif",
+		state: "Madhya Pradesh",
+		googleMapLink: 
+"https://www.google.com/maps/place/Shri+Mahakaleshwar+Jyotirlinga+Temple/@23.1827414,75.7656225,17z/data=!3m1!4b1!4m6!3m5!1s0x3963743638ffffff:0x3024e4d1bd13cc22!8m2!3d23.1828736!4d75.7682321!16zL20vMDZzaDBu?entry=ttu&g_ep=EgoyMDI1MDMwMi4wIKXMDSoASAFQAw%3D%3D",
+	},
+    {
+		id: 3,
+		date: "June 2022",
+		title: "Robber's Cave, Dehradun",
+		description: "Robber's Cave, also known as Guchhupani, is a river cave in Dehradun, Uttarakhand, India. It's a popular tourist destination known for its natural beauty and unique features.",
+		coverImg: "./images/Robbers-Cave.jpg",
+		state: "Uttrakhand",
+		googleMapLink: 
+"https://www.google.com/maps/place/GUCCHUPANI+ROBBERS+CAVE/@30.3753952,78.057282,17z/data=!3m1!4b1!4m6!3m5!1s0x3908d75eb8be4f07:0xc25c085050729e14!8m2!3d30.3753952!4d78.0598569!16s%2Fg%2F11t7_v372z?entry=ttu&g_ep=EgoyMDI1MDMwMi4wIKXMDSoASAFQAw%3D%3D",
+	},
+    {
+		id: 4,
+		date: "August 2023",
+		title: "City Palace, Udaipur",
+		description: "City Palace (Raj Mahal), Udaipur is a palace complex situated in the city of Udaipur in the Indian state of Rajasthan. It was built over a period of nearly 400 years, with contributions from several rulers of the Mewar dynasty. Its construction began in 1553, started by Maharana Udai Singh II of the Sisodia Rajput family as he shifted his capital from the erstwhile Chittor to the newfound city of Udaipur.[1] The palace is located on the east bank of Lake Pichola and has several palaces built within its complex.",
+		coverImg: "./images/CityPalace.jfif",
+		state: "Rajsthan",
+		googleMapLink: 
+"https://www.google.com/maps/place/City+Palace/@24.5764421,73.680936,17z/data=!3m1!4b1!4m6!3m5!1s0x3967e5640659a7d5:0x9257af76faeaab0e!8m2!3d24.5764421!4d73.6835109!16s%2Fm%2F047lp5x?entry=ttu&g_ep=EgoyMDI1MDMwMi4wIKXMDSoASAFQAw%3D%3D",
+	},
+    {
+		id: 5,
+		date: "November 2024",
+		title: "National Zoological Park, Delhi",
+		description: "The Delhi zoo came decades later after New Delhi was built. although the idea to have a zoo at the national capital was mooted in 1951, the park was inaugurated in November 1959.",
+		coverImg: "./images/Delhi-Zoo.avif",
+		state: "New Delhi",
+		googleMapLink: 
+"https://www.google.com/maps/place/National+Zoological+Park,+New+Delhi/@28.6062122,77.2426696,17z/data=!3m1!4b1!4m6!3m5!1s0x390ce318a027dbb9:0xf12a1c6b59580448!8m2!3d28.6062122!4d77.2452445!16s%2Fm%2F04n1lkq?entry=ttu&g_ep=EgoyMDI1MDMwMi4wIKXMDSoASAFQAw%3D%3D",
+	}
+]
